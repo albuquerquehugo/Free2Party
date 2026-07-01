809133100641452b9420808f399f5dbf2bebc28c
Hugo Albuquerque <albuquerquehugo@gmail.com>
2026-07-01 11:46:14 -0700

    version update: v14 (0.6.1) - Implemented Interests section in User Profile - Design improvements in Edit Profile screen - Added error messages to Google Sign-in - Other bug fixes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.free2party"
         minSdk = 24
         targetSdk = 37
-        versionCode = 13
-        versionName = "0.6.0"
+        versionCode = 14
+        versionName = "0.6.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
